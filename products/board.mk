d8bc170137960fb4ed9015e9310e0f35f09913f3
Pranav Vashi <neobuddy89@gmail.com>
2024-04-11 21:40:28 +0300

    Add Moto dolby support

--- products/board.mk
+++ products/board.mk
@@ -1,3 +1,11 @@
 # Build flags
 BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
+# Sepolicy
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += vendor/moto/dolby/sepolicy/public
+BOARD_VENDOR_SEPOLICY_DIRS += vendor/moto/dolby/sepolicy/vendor
+
+# VINTF
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/moto/dolby/configs/vintf/framework_compatibility_matrix.xml
+DEVICE_MANIFEST_FILE += vendor/moto/dolby/configs/vintf/manifest.xml
--- products/dolby.mk
+++ products/dolby.mk
@@ -1,1 +1,8 @@
+# Dolby
+PRODUCT_COPY_FILES += \
+    vendor/moto/dolby/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml \
+    vendor/moto/dolby/configs/dolby/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml
+
+TARGET_EXCLUDES_AUDIOFX := true
+
 $(call inherit-product, vendor/moto/dolby/products/board.mk)
